adee29e5e12573fbbd0e02e23d96b42728a7332e
MaluAbrami <marialuizaabrami@gmail.com>
2024-02-28 21:42:40 -0300

    ex 1 incompleto

---
+++ ListaFuncoes/src/index.ts
@@ -0,0 +1,11 @@
+/* ARRUMAR A PARTE DO TAMANHO DO ARRAY
+function maiorNumero(...lista:number[]):number {
+    let maior = 0;
+    for(let i=0; i <= lista[...]; i++){
+        if(maior < lista[i]){
+            maior = lista[i];
+        }
+    }
+    return maior;
+}
+*/
