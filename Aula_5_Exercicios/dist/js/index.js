b9136ef7e012b5a91976ce53a9cdc4aa86a85436
MaluAbrami <marialuizaabrami@gmail.com>
2024-03-04 19:47:59 -0300

    ex 1 finalizado e ex 2 falta corrigir

--- Aula_5_Exercicios/dist/js/index.js
+++ Aula_5_Exercicios/dist/js/index.js
@@ -1,17 +1,15 @@
-function ordenar(...numeros:number[]):number[] {
+"use strict";
+function ordenar(...numeros) {
     return numeros.sort();
 }
-
 console.log(ordenar(5, 7, 2, 3, 1));
-
-function mediaPonderada(...nota:number[]):number {
+function mediaPonderada(...nota) {
     let soma = 0, peso = 1, somaPesos = 1;
-    for(let i=0; i<nota.length; i++){
+    for (let i = 0; i < nota.length; i++) {
         soma += nota[i] * peso;
         peso++;
         somaPesos += peso;
     }
     return soma / somaPesos;
 }
-
 console.log(mediaPonderada(7, 6, 8));
