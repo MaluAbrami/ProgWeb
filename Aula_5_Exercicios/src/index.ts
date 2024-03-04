b927cc007f6851ccc7879aae3163a53da619ffc9
MaluAbrami <marialuizaabrami@gmail.com>
2024-03-04 20:08:12 -0300

    ex 3 todo errado, preciso arrumar a parte de receber um number e convertar em array

--- Aula_5_Exercicios/src/index.ts
+++ Aula_5_Exercicios/src/index.ts
@@ -15,3 +15,30 @@ function mediaPonderada(...nota:number[]):number {
 }
 
 console.log(mediaPonderada(7, 6, 8));
+
+function validarCPF(...cpf:number[]) {
+    let penultimo = cpf[9];
+    let ultimo = cpf[10];
+    let soma = 0, valor = 10;
+
+    for(let i=0; i<10; i++){
+        soma += cpf[i] * valor;
+        valor --;
+    }
+    
+    if((soma * 10) % 11 == cpf[9]){
+        valor = 11, soma = 0;
+        for(let i=0; i<11; i++){
+            soma += cpf[i] * valor;
+            valor --;
+        }
+        if((soma * 10) % 11 == cpf[10]){
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(validarCPF(5,2,9,9,8,2,4,7,2,5));
