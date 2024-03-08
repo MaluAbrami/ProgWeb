7020078b5dc9ac399e24d05837e62a18c690cd02
MaluAbrami <marialuizaabrami@gmail.com>
2024-03-08 20:06:37 -0300

    ex 3 finalizado

--- ListaClasses/src/index.ts
+++ ListaClasses/src/index.ts
@@ -76,7 +76,7 @@ class Calculadora {
             return (this.valor1 / 100) * this.valor2;
         }
         else{
-            return;
+            return 'Erro: Apenas valores positivos são permitidos';
         }
     }
 }
@@ -99,3 +99,58 @@ console.log('Operação de porcentagem, 10% de 100 = ', quintaOp.porcentagem());
 console.log('Operação de porcentagem, -5% de 100 = ', quintaOpAlternativo.porcentagem());
 
 
+class Produto {
+    nome: string;
+    preco: number;
+    quantidadeEmEstoque: number;
+
+    constructor(nome: string, preco: number, quantidadeEmEstoque: number){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    calcularValorTotalEmEstoque(): number{
+        return this.preco * this.quantidadeEmEstoque;
+    }
+
+    reporEstoque(quantidade:number):void{
+        this.quantidadeEmEstoque += quantidade;
+    }
+
+    vender(quantidade:number){
+        if(quantidade > this.quantidadeEmEstoque){
+            return '\nErro: a quantidade em estoque era menor do que a quantidade a ser vendida'
+        }
+        else{
+            this.quantidadeEmEstoque -= quantidade;
+        }
+    }
+
+    get getQuantidadeProdutos(){
+        return this.quantidadeEmEstoque;
+    }
+}
+
+const sabao = new Produto('Sabão', 3, 100);
+const shampoo = new Produto('Shampoo', 15, 70);
+const condicionador = new Produto('Condicionador', 18, 50);
+
+console.log('\nExercício 3 - Produtos');
+
+console.log('\nProdutos em estoque: \n', sabao,'\n', shampoo,'\n', condicionador);
+console.log('O valor total de produtos em estoque é ', sabao.calcularValorTotalEmEstoque() + shampoo.calcularValorTotalEmEstoque() + condicionador.calcularValorTotalEmEstoque());
+
+console.log('\nRepondo estoque de sabão com 50 unidades', sabao.reporEstoque(50));
+console.log('Repondo estoque de shampoo com 15 unidades', shampoo.reporEstoque(15));
+console.log('Repondo estoque de condicionador com 20 unidades', condicionador.reporEstoque(20));
+console.log('Produtos em estoque: \n', sabao,'\n', shampoo,'\n', condicionador);
+console.log('O valor total de produtos em estoque é ', sabao.calcularValorTotalEmEstoque() + shampoo.calcularValorTotalEmEstoque() + condicionador.calcularValorTotalEmEstoque());
+
+console.log('\nForam vendidos 30 sabões', sabao.vender(30));
+console.log('Foram vendidos 5 shampoos', shampoo.vender(5));
+console.log('Foram vendidos 10 condicionadores', condicionador.vender(10));
+console.log('Produtos em estoque: \n', sabao,'\n', shampoo,'\n', condicionador);
+console.log('O valor total de produtos em estoque é ', sabao.calcularValorTotalEmEstoque() + shampoo.calcularValorTotalEmEstoque() + condicionador.calcularValorTotalEmEstoque());
+
+console.log('\nForam vendidos 150 sabões', sabao.vender(150));
