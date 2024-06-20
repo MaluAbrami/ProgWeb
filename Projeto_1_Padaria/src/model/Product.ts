46484dec4dce3f96879bee409098d8244ed0735d
Malu Abrami <marialuizaabrami@gmail.com>
2024-06-20 00:04:59 -0300

    Entidades criadas, precisa organizar e definir as chaves estrangeiras

--- Projeto_1_Padaria/src/model/Product.ts
+++ Projeto_1_Padaria/src/model/Product.ts
@@ -1,13 +1,45 @@
-export class Product{
+export class ModalidadePaes{
     id:number;
     name:string;
-    description:string;
     price:number;
+    vegano: boolean;
 
-    constructor(name:string, description:string, price:number){
+    constructor(name:string, vegano:boolean, price:number){
         this.name = name;
-        this.description = description;
         this.price = price;
+        this.vegano = vegano;
+        this.id = this.geraId();
+    }
+
+    private geraId():number{
+        return Date.now();
+    }
+}
+
+export class EstoquePaes{
+    id:number;
+    quantidade:number;
+
+    constructor(quantidade:number){
+        this.quantidade = quantidade;
+        this.id = this.geraId();
+    }
+
+    private geraId():number{
+        return Date.now();
+    }
+}
+
+export class VendaPaes{
+    cpf:number;
+    id:number;
+    quantidade:number;
+    valor:number;
+
+    constructor(cpf:number, quantidade:number, valor:number){
+        this.cpf = cpf;
+        this.quantidade = quantidade;
+        this.valor = valor;
         this.id = this.geraId();
     }
 
