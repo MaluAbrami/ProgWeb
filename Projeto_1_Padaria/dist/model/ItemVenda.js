08e9f7daf432525687ab813411eaaf4b6a0ba79d
Malu Abrami <marialuizaabrami@gmail.com>
2024-06-28 16:54:45 -0300

    criação do model ItemVenda

---
+++ Projeto_1_Padaria/dist/model/ItemVenda.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ItemVenda = void 0;
+class ItemVenda {
+    constructor(estoquePaesId, quantidade) {
+        this.estoquePaesId = estoquePaesId;
+        this.quantidade = quantidade;
+    }
+}
+exports.ItemVenda = ItemVenda;
---
+++ Projeto_1_Padaria/src/model/ItemVenda.ts
@@ -0,0 +1,9 @@
+export class ItemVenda{
+    estoquePaesId: number;
+    quantidade: number;
+
+    constructor(estoquePaesId: number, quantidade: number){
+        this.estoquePaesId = estoquePaesId;
+        this.quantidade = quantidade;
+    }
+}
