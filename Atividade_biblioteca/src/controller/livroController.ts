dee658420261a9def14b9ceff6f38c029d336f54
MaluAbrami <marialuizaabrami@gmail.com>
2024-07-12 20:52:46 -0300

    deletarLivro

--- Atividade_biblioteca/src/controller/livroController.ts
+++ Atividade_biblioteca/src/controller/livroController.ts
@@ -58,3 +58,17 @@ export async function atualizarLivro(req: Request, res: Response) {
         res.status(400).json({ message: error.message });
     }
 };
+
+export async function deletarLivro(req: Request, res: Response) {
+    try{
+        const livro = await livroService.deletarLivro(req.body);
+        res.status(200).json(
+            {
+                mensagem: "Livro deletado com sucesso",
+                livro: livro
+            }
+        );
+    } catch(error: any){
+        res.status(400).json({ message: error.message });
+    }
+};
