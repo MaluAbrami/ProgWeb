36f36563870e98ba72263dd6ba671fa8b1b29bf8
MaluAbrami <marialuizaabrami@gmail.com>
2024-07-12 21:10:22 -0300

    endpoints

--- Atividade_biblioteca/src/app.ts
+++ Atividade_biblioteca/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { cadastrarLivro, listarTodosProdutos } from './controller/livroController';
+import { atualizarLivro, cadastrarLivro, deletarLivro, filtrarLivro, listarTodosLivros } from './controller/livroController';
 
 const app = express();
 
@@ -8,6 +8,9 @@ const PORT = 3000;
 app.use(express.json());
 
 app.post("/api/books", cadastrarLivro);
-app.get("/api/books", listarTodosProdutos)
+app.get("/api/books", listarTodosLivros);
+app.get("/api/books/:id", filtrarLivro);
+app.put("/api/books/:id", atualizarLivro);
+app.delete("/api/books/:id", deletarLivro);
 
 app.listen(PORT, ()=> console.log("API online na porta: " + PORT));
