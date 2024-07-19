e18c4e252937ef6819b6a754582e3a2f02ddcbab
MaluAbrami <marialuizaabrami@gmail.com>
2024-07-19 19:23:45 -0300

    commit

--- Atividade_biblioteca/src/controller/livroController.ts
+++ Atividade_biblioteca/src/controller/livroController.ts
@@ -5,13 +5,13 @@ const livroService = new LivroService();
 
 export async function cadastrarLivro (req: Request, res: Response) {
     try{
-            const novoLivro = await livroService.cadastrarLivro(req.body);
-            res.status(201).json(
-                {
-                    mensagem: "Livro adicionado com sucesso!",
-                    livro: novoLivro
-                }
-            );
+        const novoLivro = await livroService.cadastrarLivro(req.body);
+        res.status(201).json(
+            {
+                mensagem: "Livro adicionado com sucesso!",
+                livro: novoLivro
+            }
+        );
     } catch(error: any){
         res.status(409).json({ message: error.message });
     }
