45cb23d3bb8691d440973c77c9b84c7916f543d4
MaluAbrami <marialuizaabrami@gmail.com>
2024-07-26 20:36:59 -0300

    função cria cliente service

--- Aula_20_Relacionamento_Entidades Prática/src/service/ClienteService.ts
+++ Aula_20_Relacionamento_Entidades Prática/src/service/ClienteService.ts
@@ -7,4 +7,20 @@ export class ClienteService{
 
     clienteRepository = new ClienteRepository();
     contaRepository = new ContaRepository();
+
+    async criaCliente(clienteData: any): Promise<Cliente>{
+        const {nome, cpf, data_nascimento} = clienteData;
+
+        if( !nome || !cpf || !data_nascimento){
+            throw new Error("Informações incompletas");
+        }
+
+        const resultado: Cliente[] = await this.clienteRepository.getClientePorCpf(cpf);
+
+        if(resultado.length > 0){
+            throw new Error("Cpf já cadastrado");
+        }
+
+        return this.clienteRepository.insereCliente(new Cliente(0,nome, cpf, data_nascimento));
+    }
 }
